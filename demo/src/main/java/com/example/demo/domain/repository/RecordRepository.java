ef7d26726635aa842c4d88b9658d561b856fcd88
konishko <sdl@renue.ru>
2020-11-05 17:02:27 +0500

    fixed constructors in entities, correct mapping

--- demo/src/main/java/com/example/demo/domain/repository/RecordRepository.java
+++ demo/src/main/java/com/example/demo/domain/repository/RecordRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.domain.repository;
 import com.example.demo.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.UUID;
 @Repository
 public interface RecordRepository extends JpaRepository<Record, UUID> {
     @Query("select tt from Record tt where tt.link = :link")
-    List<Record> getAllByLink(final String link);
+    List<Record> getAllByLink(final @Param("link") String link);
 }
--- demo/src/main/java/com/example/demo/domain/repository/SubscriberRepository.java
+++ demo/src/main/java/com/example/demo/domain/repository/SubscriberRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.domain.repository;
 import com.example.demo.domain.Subscriber;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.UUID;
 @Repository
 public interface SubscriberRepository extends JpaRepository<Subscriber, UUID> {
     @Query("select tt from Subscriber tt where tt.nickname = :nickname")
-    List<Subscriber> getAllByNickname(final String nickname);
+    List<Subscriber> getAllByNickname(final @Param("nickname") String nickname);
 }
