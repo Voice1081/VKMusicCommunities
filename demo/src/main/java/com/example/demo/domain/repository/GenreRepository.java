2ab980c919e790350d6b670bf504e008c588b3a8
Данил <sdl@renue.com>
2020-12-03 13:36:52 +0500

    new genre table

---
+++ demo/src/main/java/com/example/demo/domain/repository/GenreRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain.repository;
+
+import com.example.demo.domain.Genre;
+import com.example.demo.domain.Record;
+import com.sun.tools.javac.jvm.Gen;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, String> {
+}
---
+++ demo/src/main/resources/scripts/genre.sql
@@ -0,0 +1,5 @@
+create table genre
+(
+    id text primary key,
+    name text
+)
